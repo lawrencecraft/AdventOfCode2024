71f945b932367c3f4b25f326741db1240490224b
Lawrence Craft <lawrence.craft@gmail.com>
2024-12-09 09:32:14 +0000

    optimise

--- day07/day07.py
+++ day07/day07.py
@@ -33,31 +33,24 @@ def part1(input: list[str]) -> None:
 def part2(input: list[str]) -> None:
     lines = [_parse_line(x) for x in input]
 
-    def _walk_tree(target: int, total: int | None, remaining: list[int]) -> bool:
+    def _walk_tree(target: int, total: int, remaining: list[int]) -> bool:
         if remaining == []:
             return target == total
 
         if total and total > target:
+            # print('pruned')
             return False
 
         first = remaining[0]
         rest = remaining[1:]
 
-        addition_total = total or 0
-        if _walk_tree(target, addition_total + first, rest):
-            return True
+        return (
+            _walk_tree(target, total + first, rest)
+            or _walk_tree(target, total * first, rest)
+            or _walk_tree(target, int(f"{total}{first}"), rest)
+        )
 
-        mult_total = total or 1
-        if _walk_tree(target, mult_total * first, rest):
-            return True
-
-        concat_base = total or 0
-        if _walk_tree(target, int(f"{concat_base}{first}"), rest):
-            return True
-
-        return False
-
-    print(sum(l.total for l in lines if _walk_tree(l.total, None, l.items)))
+    print(sum(l.total for l in lines if _walk_tree(l.total, l.items[0], l.items[1:])))
 
 
 def _parse_line(line: str) -> Line:
